a1d6a90b6959205a4809bde27f367dee64e9f5bc
Yue <kennyyue@byui.edu>
2019-10-31 13:51:55 -0600

    update

--- R_script/Grouping.R
+++ R_script/Grouping.R
@@ -1,14 +1,13 @@
 library(tidyverse)
-library(stats)
-library(base)
 library(dplyr)
 library(readxl)
 library(animation)
+
 student_fall2019 <- read_excel("Raw Data/student_fall2019.xlsx")
 
 student_fall2019 %>% 
   group_by(USER_Gender) %>% 
-  summarise(total = n())
+  summarise(total = n()) %>% mutate(ratio = total /sum(total))
 
 student_fall2019 %>% 
   ggplot() +
@@ -19,9 +18,34 @@ M_student <- student_fall2019 %>%
 F_student <- student_fall2019 %>% 
   filter(USER_Gender == "F")
 
-set.seed(2345)
-kmeans.ani(M_student[16:17], 10)
-kmeans.ani(F_student[16:17], 10)
-kmeans(M_student[16:17], 10)$cluster
-M_student %>% mutate(subgroup = kmeans(M_student[16:17], 10)$cluster) %>% view()
-F_student %>% mutate(subgroup = kmeans(M_student[16:17], 10)$cluster) %>% view()
+# Initially divide seperate into 10 groups
+set.seed(50)
+kmeans.ani(M_student[16:17], 50) 
+kmeans.ani(tile1[16:17], 10)
+kmeans.ani(F_student[16:17],10)
+
+# Divide the Male and Female in 50 groups and try to mix and match 
+M1 <- kmeans(M_student[16:17],50)
+M_student %>% 
+  mutate(subgroup = M1$cluster) %>% 
+  group_by(subgroup) %>% 
+  summarise(Avg_x = mean(X), Avg_y = mean(Y))
+
+F1 <- kmeans(F_student[16:17],50)
+F_student %>% 
+  mutate(subgroup = F1$cluster) %>% 
+  group_by(subgroup) %>% 
+  summarise(Avg_x = mean(X), Avg_y = mean(Y))
+
+# 1st try
+M_student %>% 
+  mutate(subgroup = kmeans(M_student[16:17], 15)$cluster) %>%
+  filter( subgroup == 1) %>% 
+  mutate(sub_subgroup = kmeans(.[16:17],10)$cluster) %>% view()
+  
+# 2nd Try
+  M_student %>% 
+    mutate(subgroup = kmeans(M_student[16:17], 15)$cluster) %>%
+    group_by(subgroup) %>% 
+    summarise( Avg_x = mean(X), Avg_y = mean(Y)) %>%  ggplot(aes(x = Avg_x, y = Avg_y)) +geom_point()
+F_student %>% mutate(subgroup = kmeans(M_student[16:17], 12)$cluster) %>% view()
